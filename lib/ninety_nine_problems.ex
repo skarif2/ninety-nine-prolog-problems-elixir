cdc9995cf28af34e2f09ce92374e798fe6a77335
Sk Arif <skarif2@gmail.com>
2018-02-28 11:51:14 +0600

    Initial Commit

---
+++ lib/ninety_nine_problems.ex
@@ -0,0 +1,18 @@
+defmodule NinetyNineProblems do
+  @moduledoc """
+  Documentation for NinetyNineProblems.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NinetyNineProblems.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
